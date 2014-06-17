9d0c1a2e9c4ffc9045f9ba21398d39831a24572d
mariofriz <builtbysplash@gmail.com>
2014-06-17 20:44:21 +0200

    Autoselect connection string

--- resources/js/settings.js
+++ resources/js/settings.js
@@ -1,9 +1,14 @@
 (function($) {
 
-$('#resetKey').click(function() {
+$('#resetKey').on("click", function () {
 	Craft.postActionRequest('megaphone/resetKey', function(response) {
 	    $('#connectionString').val(Craft.getSiteUrl() + 'admin/actions/megaphone/run?key=' + response.key);
 	});
 });
+
+$('#connectionString').attr('readonly', true);
+$('#connectionString').on("click", function () {
+	$(this).select();
+})
 
 })(jQuery);
